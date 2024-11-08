4f296e453fc1e58f4852a8e52362cf33476c047c
exe <exequielbrites8@gmail.com>
2024-11-08 19:37:25 -0300

    documentacion

--- aplicacionmensajeria/src/modelo/CompresionDecorator.java
+++ aplicacionmensajeria/src/modelo/CompresionDecorator.java
@@ -1,12 +1,22 @@
 package modelo;
 
+/** Decorador concreto que ofrece funcionalidad de compresión a un mensaje. */
 public class CompresionDecorator extends MensajeDecorator {
 
+    /** Constructor. */
     public CompresionDecorator(Mensaje mensaje) {
         super(mensaje);
     }
 
-    public String enviar() { 
+    /**
+     * Envía el mensaje comprimido.
+     * Este método sobrescribe el método {@code enviar()} de
+     * {@link MensajeDecorator}
+     * para añadir un mensaje que simula la compresión.
+     *
+     * @return El mensaje comprimido como una cadena de texto.
+     */
+    public String enviar() {
         return "Compresion(" + super.enviar() + ")";
     }
 }
--- aplicacionmensajeria/src/modelo/FormatoDecorator.java
+++ aplicacionmensajeria/src/modelo/FormatoDecorator.java
@@ -1,11 +1,21 @@
 package modelo;
 
+/** Decorador concreto que ofrece funcionalidad de formato a un mensaje. */
 public class FormatoDecorator extends MensajeDecorator {
 
+    /** Constructor */
     public FormatoDecorator(Mensaje mensaje) {
         super(mensaje);
     }
 
+    /**
+     * Env a el mensaje con formato.
+     * Este m todo sobreescribe el m todo {@code enviar()} de
+     * {@link MensajeDecorator}
+     * para a adir un mensaje que simula la aplicaci n de formato al mensaje.
+     * 
+     * @return El mensaje con formato como una cadena de texto.
+     */
     public String enviar() {
         return "Formato(" + super.enviar() + ")";
     }
