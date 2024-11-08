3e96fa6d80d2eee84ac94e8719da890f5deecd4d
Camila <camila.gomez@fceqyn.unam.edu.ar>
2024-11-08 19:52:48 -0300

    javadoc

--- aplicacionmensajeria/src/App.java
+++ aplicacionmensajeria/src/App.java
@@ -1,6 +1,15 @@
 import modelo.*;
 
 public class App {
+
+        /**
+         * Punto de entrada principal del programa.
+         *
+         * Crea un mensaje básico y lo decora con funcionalidades de encriptado, formateo y compresión.
+         * Luego, se envía el mensaje y se imprime el resultado.
+         *
+         * @param args no utiliza argumentos.
+         */
     public static void main(String[] args) {
         Mensaje mensaje = new MensajeBase("Mensaje de prueba");
         mensaje = new EncriptadoDecorator(mensaje);
